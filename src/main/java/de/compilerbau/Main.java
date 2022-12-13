6a02d76afa2c87544ede22aa45b851237d830a1e
Daschi <daschi.03@icloud.com>
2022-12-13 16:28:10 +0100

    Added sample code

--- src/main/java/de/compilerbau/Main.java
+++ src/main/java/de/compilerbau/Main.java
@@ -1,8 +1,21 @@
 package de.compilerbau;
 
+import java.io.File;
+import java.io.IOException;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.DynamicType.Builder;
+import net.bytebuddy.dynamic.DynamicType.Unloaded;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Builder<Object> builder = new ByteBuddy()
+        .subclass(Object.class)
+        .name("CreatedClass");
+    try(Unloaded<Object> make = builder.make()){
+      make.saveIn(new File("generated"));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
